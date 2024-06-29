afd633f5598f4f828e7acf9367e9fa9379d5c1aa
Martin Irarrazabal <m.irarrazabalj@gmail.com>
2024-06-29 19:05:57 -0400

    registro de usuario

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path 
 from. import views
+from .views import  register
+
 
 urlpatterns = [
     path('inicioS',views.login , name="inicioS"),
+    path('register/', register, name='register'),
 
 
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
@@ -8,3 +11,24 @@ from django.contrib.auth.decorators import login_required
 def login(request):
     context = {}
     return render(request , "login/inicioS.html",context)
+
+
+
+
+def register(request):
+    data = {
+
+        'form': CustomUserCreationForm()
+    }
+
+    if request.method == 'POST':
+        user_creation_form = CustomUserCreationForm(data=request.POST)
+
+        if user_creation_form.is_valid():
+            user_creation_form.save()
+
+            user == authenticate(username=user_creation_form.cleaned_data['username'] , password=user_creation_form.cleaned_data['password1'])
+            login(request,user)
+            return redirect('home')
+
+    return render(request, 'registration/register.html',data)
