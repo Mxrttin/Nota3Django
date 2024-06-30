881105c3acfd9889139c471aef01555f82ccf41d
Martin Irarrazabal <m.irarrazabalj@gmail.com>
2024-06-30 18:01:53 -0400

    carrito listo falta boton ver detalles

--- comicsSharingann/urls.py
+++ comicsSharingann/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path ,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,6 +25,8 @@ urlpatterns = [
     path('', include('home.urls')),
     path('carrito/', include('carrito.urls')),
     path('login/', include('login.urls')),
-    path('accounts/', include ('django.contrib.auth.urls')),
-
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
